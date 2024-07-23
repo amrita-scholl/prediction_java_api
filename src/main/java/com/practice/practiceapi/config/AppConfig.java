614158ac18bde563cf03f71a5985fa61ab671450
Amrita Singh <amrita.scholl@gmail.com>
2024-07-23 18:39:14 +0530

    initial files

--- src/main/java/com/practice/practiceapi/config/AppConfig.java
+++ src/main/java/com/practice/practiceapi/config/AppConfig.java
@@ -1,7 +1,5 @@
 package com.practice.practiceapi.config;
 
-import org.neo4j.ogm.config.Configuration;
-import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -22,14 +20,4 @@ public class AppConfig {
         return new TransactionTemplate(transactionManager);
     }
 
-    @Bean
-    public SessionFactory sessionFactory() {
-        Configuration configuration = new Configuration.Builder()
-                .uri("neo4j+s://bedcd9e5.databases.neo4j.io:7687") // Change to your Neo4j instance URI
-                .credentials("neo4j", "G0NJr1Y0I6H-E4hsms7XrMMN8VRqRJiUdk7aslHv7Ow") // Change to your Neo4j instance
-                                                                                     // credentials
-                .build();
-
-        return new SessionFactory(configuration, "com.practice.practiceapi"); // Change to your domain package
-    }
 }
